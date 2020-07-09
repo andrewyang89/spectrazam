59ed907d27b71ffdbf48c9a6105cc1a9d25145e8
sayaks1 <44211885+sayaks1@users.noreply.github.com>
2020-07-09 17:50:53 -0400

    Add files via upload

---
+++ song_titles_artists.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+from typing import List, Tuple
+
+
+
+
+song_IDs = np.array([])  # Given
+
+song_info = {}  # dictionary mapping song ID to song titles and artists
+
+def identify_song(song_ID: int):
+    """
+    Identifies and prints the song title and artist given the song ID.
+    
+    Parameters:
+    -----------
+    song_ID: int
+        The ID of the song to classify.
+        
+    """
+    song, artist = song_info[song_ID]
+    print("Song Title: {}".format(song))
+    print("Artist: {}".format(artist))
+    
+    
+    
+def add_songs(song_name_list: List[str], artist_list: List[str]):
+    """
+    Adds the list of song names and artists into the database, and 
+    updates the list of unique song IDs for each song.
+    
+    Parameters:
+    -----------
+    song_name_list: List[str]
+        A list containing a string of new song names to add
+    artist_list: List[str]
+        A corresponding list containing names of the artists
+        
+    """
+    global song_IDs
+    new_song_IDs = np.arange(len(song_IDs), len(song_IDs) + len(song_name_list))
+    song_IDs = np.arange(len(song_IDs) + len(song_name_list))  #updates song_IDs
+    
+    for song_ID in song_IDs:
+        song_info[song_ID] = (song_name_list[song_ID], artist_list[song_ID])
+
