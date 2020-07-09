99df5d4b852a618c10433c6f1c5a0cdb03d81777
Andrew Yang <andrewyang89@outlook.com>
2020-07-09 17:25:09 -0400

    Fixes the loading of spectrograms

---
+++ song_loading.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import librosa
+from pathlib import Path
+
+from IPython.display import Audio
+from microphone import record_audio
+
+import matplotlib.mlab as mlab
+
+from os import listdir
+from os.path import isfile, join
+
+
+def main():
+    music_list = load_music_files("Music/")
+    print(music_list)
+    Audio(music_list[3], rate=44100)
+    
+    spectrograms = np.array([return_specgram(samples) for samples in music_list])
+    print(spectrograms)
+
+
+def load_song_from_path(path: str):
+    """
+    Loads a song from a path.
+    
+    Returns the samples, sampling rate, and duration.
+    
+    Parameters
+    ------------
+    path: str
+          The path for the specified audio file
+          
+    Returns
+    -------
+    Tuple[np.narray, int, float]
+        The list of samples, the sampling rate and the duration of the song
+    """
+    song_path = Path(path)
+    samples, sampling_rate = librosa.load(song_path, sr=None)
+    duration = librosa.get_duration(y = samples, sr = sampling_rate)
+    
+    return samples, sampling_rate, duration
+
+
+
+
+def load_music_files(directory: str):
+    """
+    Loads the files in a folder.
+    
+    Returns the samples from each file.
+    
+    Parameters
+    ------------
+    directory: str
+          The path for the specified folder
+          
+    Returns
+    -------
+    np.ndarray size: (M,N)
+        The array of samples for each song
+    """
+    files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    music_list = np.array([load_song_from_path(directory + paths)[0] for paths in files])
+    return music_list
+
+
+
+def return_specgram(samples: np.ndarray):
+    """
+    Creates the spectrogram for a specific set of samples
+    
+    Returns the spectrogram, the frequencies, and the times
+    
+    Parameters
+    ------------
+    samples: np.ndarray
+          The array of samples
+          
+    Returns
+    -------
+    Tuple[np.narray, np.narray, np.narray]
+        The spectrogram 2d array, the frequencies and the timestamps
+    """
+    S, freqs, times = mlab.specgram(
+        samples,
+        NFFT=4096,
+        Fs=sampling_rate,
+        window=mlab.window_hanning,
+        noverlap=int(4096 / 2),
+        mode='magnitude'
+    )
+    return S, freqs, times
+
+
+def record_sound(time: float):
+    """
+    Records sound for a specific duration of time
+    
+    Returns the frames and the sample rate
+    
+    Parameters
+    ------------
+    time: float
+          The duration of the recording in seconds
+          
+    Returns
+    -------
+    Tuple[np.narray, int]
+        The frames and the sample rate
+    """
+    listen_time = time # seconds
+    frames, sample_rate = record_audio(listen_time)
+    return frames, sample_rate
+
+
+main()
